7bb4dc748e243e1516b82eec002992f0d28b896c
basdasugur <basdasugur@gmail.com>
2026-04-12 17:28:39 +0300

    README.md

--- PB2_WordGuessingGame/main.py
+++ PB2_WordGuessingGame/main.py
@@ -1,55 +1,48 @@
-# 1. Import Random
 import random
 
-# 2. Getting the User's Name and Greeting the User
-name = input("What is your name: ")
-print(f"Good Luck! {name}")
+name = input("What is your name? ")
 
-# 3. List of Words and Choosing a Random Word
-words = [
-    'rainbow', 'computer', 'science', 'programming', 'python',
-    'mathematics', 'player', 'condition', 'reverse', 'water',
-    'board', 'geeks'
-    ]
+print("Good Luck ! ", name)
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
 
 word = random.choice(words)
 
-# 4. Promting the User to Guess
+print("Guess the characters")
 
-print("\nGuess the characters")
-
-
-# 5. Initializing Guesses and Truns
 guesses = ''
 turns = 12
 
-# 6. The main game loop
-# 6.1. Checking Each Character in the Word
 while turns > 0:
+
     failed = 0
+
     for char in word:
+
         if char in guesses:
-            print(char, end= '')
+            print(char, end=" ")
+
         else:
             print("_")
             failed += 1
-            
-    # 6.2. Checking if the User Has Won
+
     if failed == 0:
         print("You Win")
-        print(f"The word is: {word}")
+        print("The word is: ", word)
         break
-    
-    # 6.3. Promting for the Next Guess
-    guess = input("guess a character: ")
+
+    print()
+    guess = input("guess a character:")
+
     guesses += guess
-    
-    # 6.4. Handling Incorrect Guesses
+
     if guess not in word:
+
         turns -= 1
         print("Wrong")
-        print(f"You have {turns}, more guesses ")
-        
-        # 6.5. Checking if the User Has Lost
+        print("You have", + turns, 'more guesses')
+
         if turns == 0:
             print("You Loose")
