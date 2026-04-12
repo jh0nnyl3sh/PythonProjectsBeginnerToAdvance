ffccdc489bbc78b2ac28942510ffaf7ad173baa1
basdasugur <basdasugur@gmail.com>
2026-04-12 17:13:48 +0300

    The main game loop

--- PB2_WordGuessingGame/main.py
+++ PB2_WordGuessingGame/main.py
@@ -3,6 +3,7 @@ import random
 name = input("What is your name: ")
 print(f"Good Luck! {name}")
 
+# List of Words and Choosing a Random Word
 words = [
     'rainbow', 'computer', 'science', 'programming', 'python',
     'mathematics', 'player', 'condition', 'reverse', 'water',
@@ -13,3 +14,17 @@ word = random.choice(words)
 
 print("\nGuess the characters")
 
+
+# Initializing Guesses and Truns
+guesses = ''
+turns = 12
+
+# The main game loop
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end= '')
+        else:
+            print("_")
+            failed += 1
