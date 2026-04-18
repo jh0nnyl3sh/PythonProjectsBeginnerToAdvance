71387dc8c83b2ca7cf8db0049492da3909326d46
basdasugur <basdasugur@gmail.com>
2026-04-18 20:55:23 +0300

    def start1()

--- PB4_21NumberGame/main.py
+++ PB4_21NumberGame/main.py
@@ -18,3 +18,79 @@ def check(xyz):
         i += 1
     return True
 
+def start1():
+    xyz = []
+    last = 0
+    while True:
+        print("Enter 'F' to take the first chance.")
+        print("Enter 'S' to take the second chance.")
+        chance = input('> ')
+
+        if chance.upper() == "F":
+            while True:
+                if last == 20:
+                    lose1()
+                print("\nYour Turn.")
+                inp = int(input("How many numbers do you wish to enter? (1-3)\n"))
+                if 1 <= inp <= 3:
+                    comp = 4 - inp
+                else:
+                    print("Wrong input. You are disqualified from the game.")
+                    lose1()
+                
+                print("Enter you numbers: ")
+                for _ in range(inp):
+                    xyz.append(int(input('> ')))
+                
+                last = xyz[-1]
+
+                if not check(xyz):
+                    print("\nYou did not enter consecutive integers.")
+                    lose1()
+                
+                if last == 21:
+                    lose1()
+
+                print("\nComputer's Trun: ")
+                for j in range(1, comp + 1):
+                    xyz.append(last + j)
+                print("Numbers after computer's turn: ", xyz)
+                last = xyz[-1]
+
+        elif chance.upper() == 'S':
+            comp = 1
+            last = 0
+            while last < 20:
+                print("\nComputer's Turn: ")
+                for j in range(1, comp + 1):
+                    xyz.append(last + j)
+                print("Numbers after computer's turn: ", xyz)
+                if xyz[-1] == 20:
+                    lose1()
+                
+                print("\nYour Turn.")
+                inp = int(input("How many numbers do you wish to enter? (1-3)\n "))
+                print("Enter your numbers: ")
+                for _ in range(inp):
+                    xyz.append(int(input('> ')))
+                last = xyz[-1]
+
+                if not check(xyz):
+                    print("\nYou did not enter consecutive integers.")
+                    lose1()
+
+                near = nearestMultiple(last)
+                comp = near - last
+                if comp == 4:
+                    comp = 3
+            
+            print("\n\nCONGURATULATIONS!!!")
+            print("YOU WON!")
+            exit(0)
+
+        else:
+            print("Wrong choice. Please enter F or S.")
+
+
+
+
